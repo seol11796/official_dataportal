67cb1f27e471f239d9c3195fb89c21bc04763312
flydog98 <guinnessoverflow@gmail.com>
2022-10-24 20:20:37 +0900

    feat: fix routing logic to accept parameters

--- routes/map.js
+++ routes/map.js
@@ -9,9 +9,25 @@ var util = require("../util");
 var complexityService = require("../service/complexityService");
 var finedustService = require("../service/finedustService");
 
-// direct request
-router.get("/", function (req, res) {
-  res.render("maps/index");
+router.get("/", async function (req, res) {
+  console.log(req.query.subway_name);
+  complex = await complexityService.getComplexity(req.query.subway_name);
+  finedust = await finedustService.getFinedust(req.query.subway_name);
+
+  console.log(complex);
+
+  res.render("maps/index", {
+    station_name: req.query.subway_name,
+    line_number: null,
+    geton_mincpx: complex[0],
+    geton_maxcpx: complex[1],
+    getoff_mincpx: complex[2],
+    getoff_maxcpx: complex[3],
+    pm: null,
+    locker_location: null,
+    nearby_building: null,
+    subway_image: null,
+  });
 });
 
 //serch
