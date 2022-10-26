7b6b5f315cce0ed23ae4f0306bae467a857ff17f
flydog98 <guinnessoverflow@gmail.com>
2022-10-26 12:18:15 +0900

    comment: add guide comment

--- service/s3Handling.js
+++ service/s3Handling.js
@@ -6,6 +6,7 @@ const bucket_name = s3_config.bucket_name;
 const access_key = s3_config.access_key;
 const secret_key = s3_config.secret_key;
 
+// S3 객체
 const S3 = new AWS.S3({
   region: "ap-northeast-2",
   credentials: {
@@ -14,16 +15,19 @@ const S3 = new AWS.S3({
   },
 });
 
+// 파일 업로드
 async function upload(stationName) {
   var object_name = stationName + ".PNG";
   var local_file_path = "./mapPictures/" + stationName + ".PNG";
   await S3.putObject({
     Bucket: bucket_name,
     Key: object_name,
+    ACL: "public-read",
     Body: fs.createReadStream(local_file_path),
   }).promise();
 }
 
+// 파일 다운로드
 async function download(stationName) {
   var object_name = stationName + ".PNG";
   var local_file_path = "./mapPictures/" + stationName + ".PNG";
